1940c65156ae20ef5285aa4c72cd457793b1956f
Koh Okada <koh.okada@gmail.com>
2016-04-22 13:25:44 +0900

    fix

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 // phina.js をグローバル領域に展開
 phina.globalize();
 
+//ブロックの縦横幅
+var BLOCK_WIDTH = 40 * 2;
+var BLOCK_HEIGHT = 60 / 2;
+
 // MainScene クラスを定義
 phina.define('MainScene', {
   superClass: 'CanvasScene',
   init: function() {
     this.superInit();
     // 背景色を指定
-    this.backgroundColor = '#444';
-    // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this);
-    this.label.x = this.gridX.center(); // x 座標
-    this.label.y = this.gridY.center(); // y 座標
-    this.label.fill = 'white'; // 塗りつぶし色
+    this.backgroundColor = 'black';
   },
 });
+
+phina.define('Block', {
+  //短形クラスを継承
+  superClass: 'RectangleShape',
+})
 
 // メイン処理
 phina.main(function() {
   // アプリケーション生成
   var app = GameApp({
-    startLabel: 'main', // メインシーンから開始する
+    startLabel: 'title', // メインシーンから開始する
   });
+
+  document.body.appendChild(app.domElement);
   // アプリケーション実行
   app.run();
 });
